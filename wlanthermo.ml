125d4de5a5de5b2f561ef615a790fc082ef436d7
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2022-03-09 11:53:33 +0100

    curl test

--- wlanthermo.ml
+++ wlanthermo.ml
@@ -1,5 +1,18 @@
 (* wlanthermo.ml -- CLI etc. for the WLANThermo API *)
 
+let curl_path = "curl"
+              
+let curl () =
+  let output, input =
+    Unix.open_process_args
+      curl_path
+      [|curl_path; "-s"; "-X"; "GET"; "http://wlanthermo/settings"|] in
+  close_out input;
+  let response = input_line output in
+  close_in output;
+  response
+
 let _ =
+  print_endline (curl ());
   ()
-    
+
