3b74619245dc839ce7f10f66742549305cfe459e
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2022-03-11 17:10:22 +0100

    generalizations

--- cli_test.ml
+++ cli_test.ml
@@ -1,16 +1,28 @@
 open Cmdliner
 
+(* int list list *)
 let channels_arg =
-  let doc = "Select the channel $(docv) (can be repeated)." in
-  Arg.(value & opt_all int [] & info ["c"; "channel"] ~docv:"N" ~doc)
+  let doc = "Select the channel(s) $(docv) (can be repeated)." in
+  let open Arg in
+  value
+  & opt_all (list int) []
+  & info ["c"; "channel"] ~docv:"N[,M...]" ~doc
 
+(* (int * int) list list *)
 let channel_ranges_arg =
   let doc = "Select the channels in the range $(docv) (can be repeated)." in
-  Arg.(value & opt_all (pair ~sep:'-' int int) [] & info ["r"; "range"] ~docv:"FROM-TO" ~doc)
+  let open Arg in
+  value
+  & opt_all (list (pair ~sep:'-' int int)) []
+  & info ["r"; "range"] ~docv:"FROM-TO" ~doc
 
+(* (float * float) option *)
 let temperature_range_arg =
   let doc = "Select the temperature range $(docv)." in
-  Arg.(value & opt (some (pair ~sep:'-' float float)) None & info ["t"; "temperature"] ~docv:"FROM-TO" ~doc)
+  let open Arg in
+  value
+  & opt (some (pair ~sep:'-' float float)) None
+  & info ["t"; "temperature"] ~docv:"FROM-TO" ~doc
 
 let range ?(stride=1) n1 n2 =
   if stride <= 0 then
@@ -38,8 +50,14 @@ let uniq = function
 let compress l =
   uniq (List.sort Stdlib.compare l)
 
-let merge_integer_ranges integers ranges =
-  compress (integers @ List.concat (List.map (fun (i, j) -> range i j) ranges))
+let expand_range (i, j) =
+  range i j
+
+let expand_ranges =
+  List.map expand_range
+
+let merge_integer_ranges integer_lists ranges =
+  compress (List.concat (integer_lists @ expand_ranges (List.concat ranges)))
 
 let temperature channels ranges =
   let all_channels = merge_integer_ranges channels ranges in
@@ -54,13 +72,15 @@ let temperature_term =
 let temperature_cmd =
   Cmd.v (Cmd.info "temperature") temperature_term
 
+type 'a switch =
+  | On of 'a
+  | Off of 'a
 
 let alarm channels channel_ranges temperature_range =
   let all_channels = merge_integer_ranges channels channel_ranges in
   Printf.printf
     "alarm of channel(s) %s set to range %s\n"
-    (String.concat
-       "," (List.map string_of_int all_channels))
+    (String.concat "," (List.map string_of_int all_channels))
     (match temperature_range with
      | None -> "?"
      | Some (t1, t2) -> "[" ^ string_of_float t1 ^ "," ^ string_of_float t2 ^ "]")
