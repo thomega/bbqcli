0666156d9d6a255351d8aebba8980595679b56c5
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2022-03-14 17:47:51 +0100

    ThoCurl: factor get/post

--- ThoCurl.ml
+++ ThoCurl.ml
@@ -22,7 +22,7 @@ let fill_buffer buffer data =
   Buffer.add_string buffer data;
   String.length data
 
-let get ?ssl ~host path =
+let do_curl ?ssl ~host path extra_setup =
   let result = Buffer.create 16384
   and error_response = ref "" in
   Curl.global_init Curl.CURLINIT_GLOBALALL;
@@ -32,6 +32,7 @@ let get ?ssl ~host path =
       Curl.set_timeout curl timeout;
       Curl.set_errorbuffer curl error_response;
       Curl.set_writefunction curl (fill_buffer result);
+      extra_setup curl;
       Curl.perform curl;
       Curl.cleanup curl
     with
@@ -45,6 +46,11 @@ let get ?ssl ~host path =
   Curl.global_cleanup ();
   Buffer.contents result
 
+let no_extras _ = ()
+
+let get ?ssl ~host path =
+  do_curl ?ssl ~host path no_extras
+
 let get_json ?ssl ~host path =
   get ?ssl ~host path |> string_to_json
 
@@ -54,37 +60,19 @@ type content =
 let content_to_string = function
   | JSON -> "application/json"
 
+(* NB: [Curl.set_post curl true] is implicit in [Curl.set_postfields].
+   It is only needed if the POST data is read with [Curl.set_readfunction].  *)
+let setup_post ?content data =
+  (fun curl ->
+    begin match content with
+    | None -> ()
+    | Some c ->
+       Curl.set_httpheader curl [ "Content-Type: " ^ content_to_string c ];
+    end;
+    Curl.set_postfields curl data)
+     
 let post ?ssl ~host path ?content data =
-  Curl.global_init Curl.CURLINIT_GLOBALALL;
-  let result = Buffer.create 16384
-  and error_response = ref "" in
-  begin try
-      let curl = Curl.init () in
-      Curl.set_url curl (url_of_path ?ssl ~host path);
-      Curl.set_timeout curl timeout;
-      Curl.set_errorbuffer curl error_response;
-      Curl.set_writefunction curl (fill_buffer result);
-      begin match content with
-      | None -> ()
-      | Some c ->
-         Curl.set_httpheader curl [ "Content-Type: " ^ content_to_string c ];
-      end;
-      Curl.set_postfields curl data;
-      (* NB: [Curl.set_post curl true] is implicit in
-         [Curl.set_postfields].  It is only needed if the
-         POST data is read with [Curl.set_readfunction].  *)
-      Curl.perform curl;
-      Curl.cleanup curl;
-    with
-    | Curl.CurlException (curlcode, _code, _msg) ->
-       Curl.global_cleanup ();
-       begin match !error_response with
-       | "" -> failwith (Curl.strerror curlcode)
-       | s -> failwith s
-       end
-  end;
-  Curl.global_cleanup ();
-  Buffer.contents result
+  do_curl ?ssl ~host path (setup_post ?content data)
 
 let post_json ?ssl ~host path data =
   post ?ssl ~host path ~content:JSON (Yojson.Basic.to_string data) |> string_to_json
