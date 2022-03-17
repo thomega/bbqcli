3741c445cce3b83313a919f430a9f715e5074c47
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2022-03-17 08:44:46 +0100

    factor Common

---
+++ Common.mli
@@ -0,0 +1,4 @@
+(* Common.ml -- options for WLANThermo API *)
+
+val man_footer : Cmdliner.Manpage.block list
+val term : ThoCurl.options Cmdliner.Term.t
--- bbqcli.ml
+++ bbqcli.ml
@@ -2,72 +2,10 @@
 
 module WT = WLANThermo
 
-let host_default = "wlanthermo"
-
 let print_json j =
   Yojson.Basic.pretty_to_string j |> print_endline
 
 open Cmdliner
-
-let man_footer =
-  [ `S Manpage.s_files;
-    `P "None, so far.";
-    `S Manpage.s_authors;
-    `P "Thorsten Ohl <ohl@physik.uni-wuerzburg.de>.";
-    `S Manpage.s_bugs;
-    `P "Report bugs to <ohl@physik.uni-wuerzburg.de>." ]
-
-
-module Common : sig val term : ThoCurl.options Term.t end =
-  struct
-
-    let docs = Manpage.s_common_options
-
-    let ssl_arg =
-      let doc = "Use SSL to connect to the host. \
-                 This option should never be necessary or even used, \
-                 because WLANThermo does not understand SSL." in
-      let env = Cmd.Env.info "WLANTHERMO_SSL" in
-      let open Arg in
-      value
-      & opt bool ~vopt:true false
-      & info ["s"; "ssl"] ~docv:"true/false" ~doc ~docs ~env
-
-    let host_arg =
-      let doc = "Connect to the host $(docv)." in
-      let env = Cmd.Env.info "WLANTHERMO_HOST" in
-      let open Arg in
-      value
-      & opt string host_default
-      & info ["H"; "host"] ~docv:"HOST" ~doc ~docs ~env
-
-    let verbose_arg =
-      let doc = "Be more verbose." in
-      let env = Cmd.Env.info "WLANTHERMO_VERBOSITY" in
-      let open Arg in
-      value
-      & opt int 0
-      & info ["v"; "verbosity"; "verbose"] ~docv:"VERBOSITY" ~doc ~docs ~env
-
-    let timeout_arg =
-      let doc = "Wait only $(docv) for response." in
-      let env = Cmd.Env.info "WLANTHERMO_TIMEOUT" in
-      let open Arg in
-      value
-      & opt (some int) None
-      & info ["T"; "timeout"] ~docv:"SECONDS" ~doc ~docs ~env
-
-    let term =
-      let open Term in
-      const
-        (fun ssl host verbosity timeout ->
-          { ThoCurl.ssl; ThoCurl.host; ThoCurl.verbosity; ThoCurl.timeout })
-      $ ssl_arg
-      $ host_arg
-      $ verbose_arg
-      $ timeout_arg
-
-  end
 
 let all_arg =
   let doc = "Include the inactive channels." in
@@ -234,7 +172,7 @@ module Alarm : Unit_Cmd =
       let man = [
           `S Manpage.s_description;
           `P "Change the temperature limits and associated alarms \
-              on a WT Mini V3 using the HTTP API." ] @ man_footer in
+              on a WT Mini V3 using the HTTP API." ] @ Common.man_footer in
       Cmd.v (Cmd.info "alarm" ~man) term
 
   end
@@ -302,7 +240,7 @@ let main_cmd =
       `P "Control a WLANThermo Mini V3 on the command line \
           using the HTTP API.";
       `S s_examples;
-      `Pre "bbqcli -c 9 -r 80-110 -p on"] @ man_footer in
+      `Pre "bbqcli -c 9 -r 80-110 -p on"] @ Common.man_footer in
   let info = Cmd.info "bbqcli" ~man in
   Cmd.group info [Alarm.cmd; Temperature.cmd; Battery.cmd;
                   Data.cmd; Settings.cmd; Info.cmd]
